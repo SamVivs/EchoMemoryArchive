cf99b5a365e89a02ea0c9a7bdf49ff49d61b505e
Sam <samuelmvivian@gmail.com>
2025-05-15 19:47:21 -0700

    Echo auto commit 2025-05-15 19:47:21

--- Code_Updates/run_memory_reflection.bat
+++ Code_Updates/run_memory_reflection.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d C:\Users\samia\OneDrive\Desktop\EchoMemoryArchive\Code_Updates
+cd /d D:\Echo_Memory_Archive\Code_Updates
 REM Run the reflection Python script
 python echo_memory_reflection.py >> reflection_log.txt 2>&1
